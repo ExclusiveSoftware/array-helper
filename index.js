1a7add5d14440fe4c7058215847d6ba8b5652673
mathewcrothers <mathew.crothers@exclusivesoftware.com.au>
2016-01-11 09:08:01 +1100

    Bug on enumerate directory

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var mods = fs.readdirSync('./lib');
+var mods = fs.readdirSync(__dirname + '/lib');
 for (var x in mods) {
     if (mods[x].slice(-3) === '.js') require('./lib/' + mods[x]);
 }
